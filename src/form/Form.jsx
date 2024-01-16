f7cf11450b12722fc2ddc40a11592e2adc18fdc4
dadondev <dadondev03@gmail.com>
2024-01-16 22:01:33 +0500

    samsas

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import styles from "./form.module.css";
 import todosMemory from "../list/todosMemory";
-import TodosList from "../list/Todos";
 
 function Form() {
   const [todos, setTodos] = useState([]);
